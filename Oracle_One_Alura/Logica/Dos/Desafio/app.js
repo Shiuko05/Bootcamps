cb7989a2de123a338bfd10d6012b8a336bdd9177
Abraham <abraham04jul@gmail.com>
2024-01-22 01:59:24 -0600

    Commit del primer punto de lógica de programación 2

---
+++ Oracle_One_Alura/Logica/Dos/Desafio/app.js
@@ -0,0 +1,1 @@
+document.querySelector('h1').innerHTML = 'Hora del Desafío';
---
+++ Oracle_One_Alura/Logica/Dos/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego de número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Adivina el número secreto entre 1 y 100';
+
+function intentoDeUsuario() {
+
+}
